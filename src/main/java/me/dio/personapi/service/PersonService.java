f7b412243136af518e7901d87c23043b0e9f3299
Rodrigo Cunha <github@docunha.com.br>
2021-11-07 15:30:01 -0300

    Developed person create operation

---
+++ src/main/java/me/dio/personapi/service/PersonService.java
@@ -0,0 +1,26 @@
+package me.dio.personapi.service;
+
+import me.dio.personapi.dto.response.MessageResponseDTO;
+import me.dio.personapi.entity.Person;
+import me.dio.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person) {
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+}
